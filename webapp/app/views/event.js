bafba04ac22d563bac35e3001c6b71144cf117a2
Alexei Shvydkiy <alexei@neovadalabs.com>
2015-02-28 14:50:18 +0300

    1 effect

--- webapp/app/views/event.js
+++ webapp/app/views/event.js
@@ -5,10 +5,26 @@ var EventView = Backbone.Marionette.ItemView.extend({
 	template: "#event",
 
 	events: {
-		'click': 'showdetails'
+		'click': 'showdetails',
+		'mouseenter': 'moredetails',
+		'mouseleave': 'lessdetails'
 	},
 
 	showdetails: function () {
 		Backbone.history.navigate("details/" + this.model.id, {trigger: true});
+	},
+
+	moredetails: function () {
+		var height = this.$el.find('.ev-header img').height();
+		this.$el.find('.ev-header').animate({
+			'margin-top': -height
+		}, 100);
+	},
+
+	lessdetails: function () {
+		this.$el.find('.ev-header').animate({
+			'margin-top': '0'
+		}, 100);
 	}
+
 });
